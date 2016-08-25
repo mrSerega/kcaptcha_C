4c48c12f5f33151f3767554bc1360d16e3778ec6
mrSerega <sega00796@mail.ru>
2016-08-25 23:15:44 +0300

    build_25082016
    объединение 3-х типов капчи в одном интерфейсе

---
+++ build_25082016_combine/defines.h
@@ -0,0 +1,6 @@
+#ifndef DEFINES_H_23082016
+#define DEFINES_H_23082016
+
+typedef unsigned long dword;
+
+#endif
---
+++ build_25082016_combine/main.cpp
@@ -0,0 +1,119 @@
+#include <string>
+#include <cstring>
+#include <ctime>
+#include <cstdlib>
+#include <windows.h>
+
+#include "dwordalphabet.h"
+#include "bytealphabet.h"
+#include "bmp.h"
+#include "genword.h"
+#include "gentext.h"
+#include "gradwave.h"
+#include "dwordtobyte.h"
+#include "createbitmap.h"
+#include "circwave.h"
+#include "gentextformono.h"
+#include "monowave.h"
+
+#include "defines.h"
+
+using namespace std;
+
+void make_grad(char* word);
+
+int main(int argc, char* argv[]){
+	//declare service var
+	int length;
+	int* word_int;
+	//declare flags
+	enum Mode_flag{CIRC,GRAD,MONO};
+		/*
+		-CIRC - CAPTCHA with circles(rus:капча с кругами)
+		-GRAD - CPATCHA with gradient(rus:капча с градиентом)
+		-MONO - monochrome CPATCHA(rus:монохромная капча)
+		*/
+	enum Rand_flag{Y,N};
+		/*
+		-Y - choose random CAPTCHA mode(rus:выбирать рандомный тип капчи)
+		-N - choose adjusted CAPTCHA mode(rus: выбирать заданный тип капчи)
+		*/
+	enum Text_flag{GEN,ADJ};
+		/*
+		-GEN - generate random text for CPTCHA(rus:генерировать рандомный текст для капчи)
+		-ADJ - use adjusted text for CAPTCHA(rus: использовать заданный текст)
+		*/
+	//init default
+	char dict[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+	string font = "font";
+	string word_str="test";
+	Mode_flag mode_flag = GRAD;
+	Rand_flag rand_flag = Y;
+	Text_flag text_flag = ADJ;
+	string filename = "captcha.bmp";
+	//init from args
+		//TODO
+	//init from config
+		//TODO
+	//init alphabets	
+	dwordalphabet dwordAlphabet(dict,font.data());
+	bytealphabet  byteAlphabet(dict,font.data()); 
+	//calculated vars
+	word_int = strToIntArr(word_str.data());
+	int wordlen = strlen(word_str.data());
+	int letterHeight = dwordAlphabet._heightOfLetter;
+	int letterWidth = dwordAlphabet._lenOfLetter;
+	int pictureWidth = letterWidth*wordlen;
+	//init bmp
+	Bmp bmp(pictureWidth,letterHeight);
+	//init backgroun colors
+	dword dwordBGC = 0xffffffff;
+	unsigned char ucBGC = 0xff;
+	//gen word if necessary
+	if(text_flag==GEN){
+		delete word_int;
+		word_int = genword(wordlen,dict);
+	}
+	//choose mode
+	if(rand_flag==Y){
+		srand(time(0));
+		mode_flag = static_cast<Mode_flag>(rand()%3);
+	}
+	//do the deal
+	switch(mode_flag){
+		case CIRC:{
+			dword* data = genText(wordlen,&dwordAlphabet,&length,word_int);
+			data = circwave(data,pictureWidth,letterHeight,dwordBGC);
+			byte* byteData = dwordToByte(data,length);
+			createBitmap(byteData,filename.data(), &bmp);
+			delete word_int;
+			delete data;
+			delete byteData;
+			break;
+		}
+		case GRAD:{
+			dword* data = genText(wordlen,&dwordAlphabet,&length,word_int);
+			data = gradwave(data,pictureWidth,letterHeight,dwordBGC);
+			byte* byteData = dwordToByte(data,length);
+			createBitmap(byteData,filename.data(), &bmp);
+			delete word_int;
+			delete data;
+			delete byteData;
+			break;
+		}
+		case MONO:{
+			unsigned char* data = gentextformono(wordlen,&byteAlphabet,word_int);
+			data = monowave(data,pictureWidth,letterHeight,ucBGC);
+			createBitmap(data,filename.data(),&bmp);
+			delete word_int;
+			delete data;
+			break;
+		}
+		default:{
+			//TODO
+			break;
+		}
+	}
+	//clear a memory
+	return 0;	
+}
